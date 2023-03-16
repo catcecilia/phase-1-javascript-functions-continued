3c92548a86177fe163fbaa92f147bdc42c0e8e76
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-03-16 14:15:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(s = 'roller-skate'){
+    return `This Saturday, I want to ${s}!`;
+}
+
+function mondayWork(s='go to the office'){
+    return `This Monday, I will ${s}.`;
+}
+
+function wrapAdjective(w='*'){
+    const wrap = w;
+    return function(s='special'){
+        return `You are ${wrap}${s}${wrap}!`;
+    }
+    
+}
+
+let result = wrapAdjective('*');
+let emphatic = result("a hard worker");
